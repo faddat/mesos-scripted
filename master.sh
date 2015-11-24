9f25dfbd83e00a2beea4bedb65b9aaf82a4e729d
Jacob Gadikian <faddat@gmail.com>
2015-11-24 22:29:03 +0700

    formalized and improved

--- master.sh
+++ master.sh
@@ -1,9 +1,11 @@
+#!/bin/bash
+cd ~/
 echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" | tee /etc/apt/sources.list.d/webupd8team-java.list
 echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" | tee -a /etc/apt/sources.list.d/webupd8team-java.list
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886
 apt-get update
 apt-get install oracle-java9-installer subversion apt-transport-https git build-essential autoconf dh-autoreconf software-properties-common python-software-properties libcurl curl autotools-dev automake libsvn-dev libapr1 libapr1-dev libtool build-essential python-dev python-boto libcurl4-nss-dev libsasl2-dev maven libapr1-dev libsvn-dev sbt aptitude clang
-git clone https://git-wip-us.apache.org/repos/asf/mesos.git
+git clone https://git-wip-us.apache.org/repos/asf/mesos.git --depth=1
 wget -q http://debian.freelan.org/debian.freelan.org.key -O- | apt-key add -
 echo "deb http://debian.freelan.org/ jessie main" >> /etc/apt/sources.list
 git clone https://github.com/bagder/curl
@@ -12,10 +14,13 @@ make -j 17
 make install
 wget http://mirrors.viethosting.vn/apache/subversion/subversion-1.9.2.tar.gz
 tar xvf subversion-1.9.2.tar.gz
+cd subversion-1.9.2
+./autogen.sh
+./configure
+make
+make install
 gvm use go1.5 --default
 go get github.com/docker/swarm
-
-
 wget get.docker.com
 sh index.html
 cd mesos
