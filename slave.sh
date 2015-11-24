7b56527e46310dfbd8deae7fe7d69c077513243e
Jacob Gadikian <faddat@gmail.com>
2015-11-24 23:32:36 +0700

    openssl and openssl-dev

--- slave.sh
+++ slave.sh
@@ -4,8 +4,9 @@ echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" | tee /e
 echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" | tee -a /etc/apt/sources.list.d/webupd8team-java.list
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886
 apt-get update
-apt-get install oracle-java9-installer apt-transport-https git build-essential autoconf libaprutil1 libaprutil1-dbg libaprutil1-dev dh-autoreconf software-properties-common python-software-properties autotools-dev automake libsvn-dev libapr1 libapr1-dev libtool build-essential libltdl-dev libltdl7 libtool libtool-bin libtool-doc libtoolkit-perl libtools-logging-clojure libtools-macro-clojure python-dev python-boto libcurl4-nss-dev libsasl2-dev maven libapr1-dev libsvn-dev aptitude clang
+apt-get install oracle-java9-installer apt-transport-https git build-essential autoconf libaprutil1 mercurial subversion bazaar openssl openssl-dev libaprutil1-dbg libaprutil1-dev dh-autoreconf software-properties-common python-software-properties autotools-dev automake libsvn-dev libapr1 libapr1-dev libtool build-essential libltdl-dev libltdl7 libtool libtool-bin libtool-doc libtoolkit-perl libtools-logging-clojure libtools-macro-clojure python-dev python-boto libcurl4-nss-dev libsasl2-dev maven libapr1-dev libsvn-dev aptitude clang
 git clone https://git-wip-us.apache.org/repos/asf/mesos.git --depth=1
+mkdir ~/mesos/build
 wget -q http://debian.freelan.org/debian.freelan.org.key -O- | apt-key add -
 echo "deb http://debian.freelan.org/ jessie main" >> /etc/apt/sources.list
 wget get.docker.com
@@ -17,9 +18,12 @@ cd subversion-1.9.2
 ./configure
 make
 make install
+gvm use go1.5 --default
+go get github.com/docker/swarm
+cd ~/mesos/build
+make clean
 cd ~/mesos
 ./bootstrap
-mkdir build
 cd build
 ../configure
 make -j 17
